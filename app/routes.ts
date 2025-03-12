b4933d7ba9d25ee3916386e637a5a05dc26cbf91
usaid AKA <ujedkemal@gmail.com>
2025-03-12 23:44:39 +0700

    first commit

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,16 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  route,
+  prefix,
+} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  route("about", "routes/about.tsx"),
+
+  ...prefix("posts", [
+    index("routes/posts.tsx"),
+    route(":post_id", "routes/post.tsx"),
+  ]),
+] satisfies RouteConfig;
